c4e70f218dc0b46065a76ce1915e1123537a1d8c
bradhilton <brad.hilton.nw@gmail.com>
2015-04-18 20:50:45 -0600

    Initial Commit

---
+++ HttpRequest/HttpRequest.h
@@ -0,0 +1,19 @@
+//
+//  HttpRequest.h
+//  HttpRequest
+//
+//  Created by Bradley Hilton on 4/18/15.
+//  Copyright (c) 2015 Skyvive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HttpRequest.
+FOUNDATION_EXPORT double HttpRequestVersionNumber;
+
+//! Project version string for HttpRequest.
+FOUNDATION_EXPORT const unsigned char HttpRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HttpRequest/PublicHeader.h>
+
+
